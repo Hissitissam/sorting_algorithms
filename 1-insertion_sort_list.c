f1e2f179e011e5995d7acaefff4efc2fb31a91dc
Hissitissam <tissam.hisssi@gmail.com>
2024-02-19 10:57:23 -0800

    update insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,24 +1,25 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * swap_n - Swap two nodes in a  doubly-linked list.
+ * @head: A pointer to the head of the doubly-linked list.
+ * @first_n: A pointer to the first node to swap.
+ * @second_n: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+
+void swap_n(listint_t **head, listint_t **first_n, listint_t *second_n)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*first_n)->next = second_n->next;
+	if (second_n->next != NULL)
+		second_n->next->prev = *first_n;
+	second_n->prev = (*first_n)->prev;
+	second_n->next = *first_n;
+	if ((*first_n)->prev != NULL)
+		(*first_n)->prev->next = second_n;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*head = second_n;
+	(*first_n)->prev = second_n;
+	*first_n = second_n->prev;
 }
 
 /**
@@ -28,20 +29,21 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  *
  * Description: Prints the list after each swap.
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *i, *insert, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (i = (*list)->next; i != NULL; i = temp)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		temp = i->next;
+		insert = i->prev;
+		while (insert != NULL && i->n < insert->n)
 		{
-			swap_nodes(list, &insert, iter);
+			swap_n(list, &insert, i);
 			print_list((const listint_t *)*list);
 		}
 	}
